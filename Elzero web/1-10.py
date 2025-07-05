720b112e9d8f43dd5c81131ff6ce25d59b76a23f
theonlydeehere <mohamedmadel8@gmail.com>
2025-07-05 19:40:22 +0300

    Add files via upload

---
+++ Elzero web/1-10.py
@@ -0,0 +1,14 @@
+Name = "Mohamed"
+Age = "22"
+Country = "Egypt"
+#############################################
+print("Name:", Name)
+print("Age:", Age) 
+print("Country:", Country)
+#############################################
+print(f"Hello, My Name Is {Name} And Iam {Age} Years Old and I Live in {Country}.")
+#############################################
+print(type(Name))
+print(type(Age))
+print(type(Country))
+#############################################
